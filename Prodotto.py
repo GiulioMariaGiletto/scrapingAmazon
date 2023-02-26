5d01277392b5c3f870ce00f240e1b3519aee5781
giulio <s269975@studenti.polito.it>
2023-02-26 23:30:49 +0100

    third attempt

--- Prodotto.py
+++ Prodotto.py
@@ -1,16 +1,27 @@
-class Prodotto:
+class Prodotto :
+
     def __init__(self, titolo, prezzo, url, rating, nReview):
         self.titolo = titolo
         self.prezzo = prezzo
         self.url = url
         self.rating = rating
         self.nReview = nReview
+        self.punteggio = (prezzo*10+rating*8+nReview*6)/24
 
     def __hash__(self) -> int:
         return super().__hash__()
 
     def __str__(self) -> str:
-        return super().__str__()
+        return self.titolo
+
+    def __eq__(self, o: object) -> bool:
+        return super().__eq__(o)
+
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -6,74 +6,75 @@ from selenium import webdriver
 from Prodotto import Prodotto
 
 prodotti = []
-
+global donny
+global pickprod
 def main():
 
     driver = webdriver.Chrome()
+    pickprod = input(" Select a product: ")
+    for page in range(1, 21):
+        if page == 1:
+            url = get_url(pickprod)
+            driver.get(url)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            results = soup.find_all('div', {'data-component-type': 's-search-result'})
+        else:
+            url = turn_page(pickprod, page)
+            driver.get(url)
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+            results = soup.find_all('div', {'data-component-type': 's-search-result'})
 
-    url = "https://www.amazon.com/"
-    global donny
 
 
-    get_url("garden gloves")
 
-    driver.get(get_url("garden gloves"))
-
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    results = soup.find_all('div', {'data-component-type': 's-search-result'})
     for item in results:
         extract_record(item)
     print(len(results))
 
-'''
+    for prod in prodotti:
+        print(prod)
 
-    item = results[0]
-    atag = item.h2.a
-    print(atag.text)
-    url = 'https://www.amazon.com/'+atag.get('href')
-    price_parent = item.find('span', 'a-price')
-    price_parent = price_parent.find('span', 'a-offscreen')
-    print(price_parent.text)
-    rating = item.i.text[0:3]
-    print(rating)
-    print(item.i.text)
-    review_count = item.find('span', {'class' : 'a-size-base s-underline-text'}).text
-    r1 = review_count.replace('(','')
-    r1 = r1.replace(')','')
-    r1 = r1.replace(',', '.')
-    num1 = float(r1)
-
-
-    print(num1)
-
-'''
-
-
-
-
-
+    return
 
 
 def extract_record(item):
+    if type(item) == 'NoneType':
+        return
     atag = item.h2.a
     titolo = atag.text
     url = 'https://www.amazon.com/' + atag.get('href')
-    price_parent = item.find('span', 'a-price')
-    price_parent = price_parent.find('span', 'a-offscreen')
-    if price_parent.text == '':
+    try:
+        price_parent = item.find('span', 'a-price')
+        price_parent = price_parent.find('span', 'a-offscreen')
+        prezzo = float(price_parent.text.replace('$', ''))
+        print(price_parent.text)
+    except AttributeError:
         return
-    prezzo = int(price_parent.text.replace('$', ''))
-    print(price_parent.text)
-    rating = int(item.i.text[0:3])
+
+    try:
+        if item.i.text[0:3] == '':
+            rating = 0.0
+        else:
+            rating = float(item.i.text[0:3])
+    except AttributeError:
+        rating = 0.0
     print(rating)
-    print(item.i.text)
-    review_count = item.find('span', {'class': 'a-size-base s-underline-text'}).text
-    r1 = review_count.replace('(', '')
-    r1 = r1.replace(')', '')
-    r1 = r1.replace(',', '.')
+
+    try:
+        print(item.i.text)
+        review_count = item.find('span', {'class': 'a-size-base s-underline-text'}).text
+        r1 = review_count.replace('(', '')
+        r1 = r1.replace(')', '')
+        r1 = float(r1.replace(',', '.'))
+    except AttributeError:
+        r1 = 0.0
     prod = Prodotto(titolo, prezzo, url, rating, r1)
     prodotti.append(prod)
 
+    return
+
+def sorting_product(List):
+    prodotti.sort(Prodotto.punteggio)
     return
 
 
@@ -83,6 +84,16 @@ def get_url(string):
         string = string.replace(' ', '+')
     donny = base.format(prodotto = string)
     return donny
+
+
+def turn_page(string, page):
+    if " " in string:
+        string = string.replace(' ', '+')
+    base = 'https://www.amazon.com/s?k='+string+'&page={a}&crid=CIO0PKVSSOAI&qid=1677448063&sprefix='+string+'%2Caps%2C482&ref=sr_pg_{b}'
+    newy = base.format(a = page)
+    newy = newy.format(b = page)
+
+    return newy
 
 
 
